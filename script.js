c59830417734763e8a91f463777c68f4fce06402
suyash <suyashjai05@gmail.com>
2021-04-23 15:55:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span >
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                   Delete
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){ 
+            //console.log(current_tasks[i]);
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        document.querySelector("#newtask input").value = ""; 
+    }
+}
